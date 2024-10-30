edfd54b595bcddff05d5fdb586d200f6e663ebf6
Vigo Putra <artupogiv@gmail.com>
2024-10-30 11:03:32 +0700

    feat: modify tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ['./public/index.html',
+    './public/css/index.css',
     './src/**/*.{js,jsx,ts,tsx}'],
   theme: {
     extend: {
